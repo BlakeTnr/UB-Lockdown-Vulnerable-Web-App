9f48b9b08309a94d2523f7cb3e911ad30462ad48
BlakeTnr <blake@academiki.com>
2022-02-25 07:35:06 -0500

    router setup

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue'
+import HoursView from '../views/HoursView.vue'
+import OrderView from '../views/OrderView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/hours',
+    name: 'hours',
+    component: HoursView
+  },
+  {
+    path: '/order',
+    name: 'order',
+    component: OrderView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
   }
 ]
 
---
+++ webapp/src/views/OrderView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <p>orders</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
