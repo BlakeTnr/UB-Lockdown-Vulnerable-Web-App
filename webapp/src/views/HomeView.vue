f39f0f6ba137b2f80cddcbe91a697a50600b2f73
BlakeTnr <blake@academiki.com>
2022-02-24 21:35:30 -0500

    App bar and Carousel

--- webapp/src/views/HomeView.vue
+++ webapp/src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-carousel cycle>
+      <v-carousel-item :src='this.$data.pizza1'>
+      </v-carousel-item>
+      <v-carousel-item :src='this.$data.pizza2'>
+      </v-carousel-item>
+      <v-carousel-item :src='this.$data.pizza3'>
+      </v-carousel-item>
+      <v-carousel-item :src='this.$data.pizza4'>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import pizza1 from '../assets/pizza1.jpg'
+import pizza2 from '../assets/pizza2.jpg'
+import pizza3 from '../assets/pizza3.jpg'
+import pizza4 from '../assets/pizza4.png'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+
+    data() {
+      return {
+        pizza1,
+        pizza2,
+        pizza3,
+        pizza4
+      }
+    },
   }
-}
 </script>
